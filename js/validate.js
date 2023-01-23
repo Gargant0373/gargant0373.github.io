8e76d5e974233ec24d04c7614743b13b6e642fb9
Gargant <gargant0373@gmail.com>
2023-01-23 15:38:55 +0100

    added html tags for message placeholders and made script work

--- js/validate.js
+++ js/validate.js
@@ -1,7 +1,9 @@
 function validate() {
   var emailField = document.getElementsByName("email-field")[0];
   var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  var emailMessage = document.getElementById("email-message");
+  console.log(emailMessage)
   emailRegex.test(emailField.value)
-    ? (emailField.style.border = "1px solid green")
-    : (emailField.style.border = "1px solid red");
+    ? (emailMessage.innerHTML = "Looks good!")
+    : (emailMessage.innerHTML = "Please enter a valid email address.");
 }
