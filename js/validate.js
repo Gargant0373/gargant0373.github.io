12d291c74eb582497dfdbb233b210e9e487fd005
Gargant <gargant0373@gmail.com>
2023-01-23 15:42:40 +0100

    split validation into 2 functions

--- js/validate.js
+++ js/validate.js
@@ -2,8 +2,17 @@ function validate() {
   var emailField = document.getElementsByName("email-field")[0];
   var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   var emailMessage = document.getElementById("email-message");
-  console.log(emailMessage)
   emailRegex.test(emailField.value)
-    ? (emailMessage.innerHTML = "Looks good!")
-    : (emailMessage.innerHTML = "Please enter a valid email address.");
+    ? valid(emailField, emailMessage)
+    : invalid(emailField, emailMessage);
+}
+
+function valid(element, message) {
+    element.style.border = "1px solid green";
+    message.innerHTML = "Looks good!";
+}
+
+function invalid(element, message) {
+    element.style.border = "1px solid red";
+    message.innerHTML = "Please enter a valid email address.";
 }
